9329eea99c0ffc6a4940bb5b46973ee566d390f1
girish-ux <girishrathod457@gmail.com>
2022-04-28 12:43:35 +0530

    Initial commit

---
+++ 01_hello.py
@@ -0,0 +1,22 @@
+print("hello world")  #single line printing 
+
+print(''' yoo ,            #use ( '''   ''')-> multi line printing
+          boy
+         hjgj
+        jjgk
+         kgkk.''')
+
+
+print(""""                      
+      this is muilti 
+    line
+     printing 
+       """) 
+
+"""
+"""
+
+# using Module
+ # -> terminal   -> pip install (Module name) playsound
+ # from playsound    import playsound  add at top   
+ # search modules at pypi.org
---
+++ 02_variables_AND_Data_types.py
@@ -0,0 +1,41 @@
+#----- NUMBERS----
+
+# print( type(2 + 1))
+# print(5 - 1)
+# print(2*4)
+# print( type (6 / 36))
+
+# print((20-1.2))
+#print((2 ** 3))        #raise to sign
+# print(2 ** 2)
+
+# print(5 // 2)        # double divide returns round up int quotient
+# print(7 % 4)         #returns reminder
+
+
+# #MATH FUNCTION
+print(round(3.4))
+print(abs(-12))  #prints always +ve no.s
+print(pow(2,2))
+
+
+
+# Operator Precedance (  (), **,  * /,  + -)
+# print((20-5) + 4**1) 
+
+
+
+#variables 
+
+# user4_iq = 1000
+# print(user4_iq)
+
+# a,b,c = 1,2,3
+# print(b)
+# print(c)
+
+# IQ = 100;
+# user_iq = IQ / 5;  # iq/5 -> expression while the whole line is statement
+# print(user_iq)
+# d= "hello"
+# print(round(user_iq))
